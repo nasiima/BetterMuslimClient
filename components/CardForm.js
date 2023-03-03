8de548d9d7628715caa9ad6481dcb558930e4db4
“nasiima” <nasiima.mohamed1@gmail.com>
2023-03-03 12:27:00 +0000

    placeholder card

--- components/CardForm.js
+++ components/CardForm.js
@@ -48,29 +48,35 @@ export default function CardForm(){
 
   return (
     <View>
-      <TextInput
+       <TextInput
         placeholder="Card number"
         value={cardNumber}
         onChangeText={setCardNumber}
+        style={styles.input}
       />
       <TextInput
         placeholder="Cardholder name"
         value={cardholderName}
         onChangeText={setCardholderName}
+        style={styles.input}
       />
       <TextInput
         placeholder="Expiration date"
         value={expirationDate}
         onChangeText={setExpirationDate}
+        style={styles.input}
       />
       <TextInput
         placeholder="CVV"
         value={cvv}
         onChangeText={setCvv}
+        style={styles.input}
       />
       <Button
         title="Save card"
         onPress={saveCard}
+        style={styles.button}
+        textStyle={styles.buttonText}
       />
       <View style={styles.profileButtonContainer}>
         <Button title="Profile" onPress={handleProfile} />
@@ -97,16 +103,30 @@ CardForm.navigationOptions = screenProps => ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+  
   },
   profileButtonContainer: {
     position: 'absolute',
     top: 0,
     left: 0,
   },
-  text: {
-    fontSize: 20,
-    margin: 20,
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: '#008080',
+    borderRadius: 4,
+    padding: 12,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
