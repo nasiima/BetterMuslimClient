822e45e552ba93c87791b9ef96e0a2bec1c356bb
“nasiima” <nasiima.mohamed1@gmail.com>
2023-04-08 21:56:06 +0100

    install stripe update endpoint name

--- App.js
+++ App.js
@@ -6,22 +6,30 @@ import Profile from './components/Profile';
 import TheSlider from './components/TheSlider';
 import Charities from './components/Charities';
 
-
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
+const STRIPE_KEY = 'pk_test_51MicwDLyEjHx5TfaLdhZuHKvZqAGnzfsnvk4bSRavMG71Wm8mqi8KQFoUlK49GoJm4hnaZyAjhlRyLko3Y1ahkpb00a9oLo3XU'
 
+const AppNavigator = createStackNavigator(
+  {
+    Login: { screen: Login },
+    Register: { screen: Register },
+    CardForm: { screen: CardForm },
+    Profile: { screen: Profile },
+    Charities: { screen: Charities },
+    TheSlider: { screen: TheSlider },
+  }
+ 
+);
 
-const AppNavigator = createStackNavigator({
-  Login: {screen: Login},
-  Register: {screen: Register},
-  CardForm: {screen: CardForm},
-  Profile: {screen: Profile},
-  Charities: {screen: Charities},
-  TheSlider: {screen: TheSlider},
-})
+const AppContainer = createAppContainer(AppNavigator);
 
-
-const App = createAppContainer(AppNavigator);
+const App = () => (
+  <StripeProvider publishableKey={STRIPE_KEY}>
+    <AppContainer />
+  </StripeProvider>
+);
 
 export default App;
